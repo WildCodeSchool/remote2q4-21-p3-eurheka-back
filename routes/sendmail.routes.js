a7103c4630d62f59adbee39a528d338b8f74a6cc
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-02-28 15:37:05 +0100

    refactoring code

--- routes/sendmail.routes.js
+++ routes/sendmail.routes.js
@@ -1,43 +1,43 @@
 const router = require("express").Router();
-const nodemailer=require('nodemailer');
-require('dotenv').config({path:'../.env'});
+const nodemailer = require('nodemailer');
+require('dotenv').config({ path: '../.env' });
 const Joi = require('joi');
 const { sendMail } = require("../utils/mail");
 
 const validate = (data) => {
-    
+
     return Joi.object({
-      email: Joi.string().email().max(255).presence('required'),
-      lastname: Joi.string().presence('required'),
-      firstname: Joi.string().presence('required'),
-      subject: Joi.string().presence('required'),
-      message: Joi.string().presence('required'),
+        email: Joi.string().email().max(255).presence('required'),
+        lastname: Joi.string().presence('required'),
+        firstname: Joi.string().presence('required'),
+        subject: Joi.string().presence('required'),
+        message: Joi.string().presence('required'),
     }).validate(data, { abortEarly: false }).error;
-  };
+};
 
 router.post('/', async (req, res) => {
-    const errors=validate(req.body);
-    if(errors){
+    const errors = validate(req.body);
+    if (errors) {
         console.log(errors.details);
         return res.sendStatus(422);
-       
+
     }
-    try{
-        const { email,lastname,firstname,subject,message}=req.body;
+    try {
+        const { email, lastname, firstname, subject, message } = req.body;
         //
-        sendMail(subject,message,email,lastname,firstname)
-        .then((result)=>{
-            //result contains the delivery mail
-            res.status(200).send('email sent');
-        })
-        .catch((err)=>{
-            console.log(err);
-            res.status(500).send(`SMTP server not found err: ${err.errno}`);
-        })
+        sendMail(subject, message, email, lastname, firstname)
+            .then((result) => {
+                //result contains the delivery mail
+                res.status(200).send('email sent');
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).send(`SMTP server not found err: ${err.errno}`);
+            })
     }
-    catch(err){
+    catch (err) {
         console.error(err);
         res.sendStatus(500);
     };
 });
-module.exports=router;
+module.exports = router;
--- utils/mail.js
+++ utils/mail.js
@@ -1,26 +1,26 @@
-const nodemailer=require('nodemailer');
-require('dotenv').config({path:'../.env'});
+const nodemailer = require('nodemailer');
+require('dotenv').config({ path: '../.env' });
 
-async function sendMail(subject,message,sender,senderName,senderFirstName) {
-    let transporter=nodemailer.createTransport({
-        host:process.env.MAIL_SERVER,
-        port:process.env.MAIL_PORT,
-        secure:false,
-        auth:{
-            user:process.env.MAIL_SENDER,
-            pass:process.env.MAIL_SENDER_PASSWORD
+async function sendMail(subject, message, sender, senderName, senderFirstName) {
+    let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_SERVER,
+        port: process.env.MAIL_PORT,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_SENDER,
+            pass: process.env.MAIL_SENDER_PASSWORD
         },
     });
-    let messageMod=`Mail du site eurheka!\r\nMr / Mme ${senderFirstName} ${senderName} (${sender}) a envoyé ce message\r\n`+message;
+    let messageMod = `Mail du site eurheka!\r\nMr / Mme ${senderFirstName} ${senderName} (${sender}) a envoyé ce message\r\n` + message;
     let info = await transporter.sendMail({
-        from:process.env.MAIL_SENDER,
-        to:process.env.MAIL_DEST,
-        subject:subject,
-        text:messageMod
+        from: process.env.MAIL_SENDER,
+        to: process.env.MAIL_DEST,
+        subject: subject,
+        text: messageMod
     });
-    return info.accepted[0]; 
-  }
-  
-module.exports={
+    return info.accepted[0];
+}
+
+module.exports = {
     sendMail
 } 
