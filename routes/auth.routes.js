24fd3d44b8ac1f25806f006b4f17440b69005382
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-02 18:54:08 +0100

    adding auth route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const auth = require('../models/auth.model');
+const { checkLevel } = require('../middleware/UserValidation');
 
-router.post('/', (req, res) => {
+router.get('/',checkLevel ,(req, res) => {
+    let userId=req.userData.user_id;
+    let userLevel=req.userData.user_level;
+    if(userId===undefined){
+        userId=0;
+    }
+    return res.status(200).json({userId,userLevel});
 
 });
 
