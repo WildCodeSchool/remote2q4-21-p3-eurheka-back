467bd17b31bab5c9007b08472053da0fbb66c448
Florent-Lefevre <lefevre.flo@gmail.com>
2022-03-07 12:34:39 +0100

    add enterprise post and get

--- models/enterprises.model.js
+++ models/enterprises.model.js
@@ -3,6 +3,25 @@ const Joi = require('joi');
 
 const db = connection.promise();
 
+const validate = (data, forCreation = true) => {
+    const presence = forCreation ? 'required' : 'optional';
+    return Joi.object({
+        name: Joi.string().max(255).presence(presence),
+        address: Joi.string().max(255).presence(presence),
+        id_activity: Joi.number().integer().presence(presence),
+        nb_employes: Joi.number().integer().presence(presence)
+    }).validate(data, { abortEarly: false }).error;
+}
+
+const findAll = () => {
+    return db
+        .query("SELECT * FROM enterprise")
+        .then(([result]) => {
+            return result
+        })
+        .catch((err) => console.error(err));
+}
+
 const findOne = (id) => {
     return db
         .query("SELECT * FROM enterprise WHERE id_enterprise=?", [id])
@@ -20,7 +39,21 @@ const findOneByName = (EnterpriseName) => {
             return result[0]
         });
 }
+
+const createOne =  (data) => {
+    const {name,id_activity,nb_employes,address}=data;
+    return db
+        .query("INSERT INTO enterprise (name,adress,id_activity,Nb_employes) VALUES (?,?,?,?)", [name, address, id_activity, nb_employes])
+        .then(([result]) => {
+            const idEnterprise = result.insertId;
+            return idEnterprise;
+        })
+}
+
 module.exports = {
     findOne,
     findOneByName,
+    validate,
+    createOne,
+    findAll
 }
--- routes/enterprises.routes.js
+++ routes/enterprises.routes.js
@@ -1,17 +1,39 @@
 const router = require("express").Router();
 const enterprise = require('../models/enterprises.model');
+const { userCheck, checkLevel, checkAdmin} = require('../middleware/UserValidation');
+const { userRole } = require('../utils/definitions')
 
-router.get('/',(req,res)=>
+router.get('/', userCheck, checkAdmin, async (req,res)=>
 {
-
+    // get enterprises data
+    const enterprises = await enterprise.findAll();
+    if (enterprises)
+    return res.status(200).json(enterprises);
+    else
+    return res.sendstatus(500);
 });
 router.get('/:id',(req,res)=>
 {
 
 });
-router.post('/',(req,res)=>
+router.post('/', userCheck, async (req,res)=>
 {
-
+    // check data
+    const error = enterprise.validate(req.body);
+    if (error) {
+        const errorDetail = error.details;
+        const errorArray = [];
+        errorDetail.forEach((item) => {
+            errorArray.push(item.message);
+        } )
+        return res.status(422).json(errorArray);
+    }
+    const idEnterprise = await enterprise.createOne(req.body);
+    if (idEnterprise) {
+        return res.status(201).json({idEnterprise});
+    } else {
+        return res.sendstatus(500)
+    }
 });
 router.put('/:id',(req,res)=>
 {
