0dbc94d087f4f60e950a469903172dbfe6621921
Florent-Lefevre <lefevre.flo@gmail.com>
2022-03-23 15:44:41 +0100

    add post cv

--- models/cvs.model.js
+++ models/cvs.model.js
@@ -3,6 +3,13 @@ const Joi = require('joi');
 
 const db = connection.promise();
 
+const validate = (data, forCreation = true) => {
+    const presence = forCreation ? 'required' : 'optional';
+    return Joi.object({
+        path: Joi.string().max(255).presence(presence)
+    }).validate(data, { abortEarly: false }).error;
+}
+
 const findAll = () => {
     return db
         .query("SELECT u.firstname, u.lastname, u.email, cv.path FROM users as u INNER JOIN cv_to_user as t ON id_user=u.id_users INNER JOIN cv ON cv.id_cv=t.id_cv")
@@ -22,7 +29,34 @@ const findOne = (id) => {
     .catch((err) => console.error(err));
 }
 
+const create=(path)=>{
+    return db
+        .query("INSERT INTO cv (path) VALUES (?)",[path])
+        .then(([result])=>{
+            return result.insertId;
+        })
+        .catch((err) => {
+            console.log(err);
+            return err;
+        });
+}
+
+const add_CvToUser=(id_user,id_cv,is_owner)=>{
+    return db
+        .query("INSERT INTO cv_to_user (id_cv,id_user,is_owner) VALUES(?,?,?)",[id_cv,id_user,is_owner])
+        .then(([result])=>{
+            return result.affectedRows!==0;
+        })
+        .catch((err)=>{
+            console.log(err);
+            return err;
+        })
+} 
+
 module.exports = {
     findAll,
-    findOne
+    findOne,
+    create,
+    add_CvToUser,
+    validate
 }
--- routes/cvs.routes.js
+++ routes/cvs.routes.js
@@ -1,17 +1,40 @@
 const router = require("express").Router();
+const multer = require('multer');
 const cv = require('../models/cvs.model');
 const { userCheck, checkLevel, checkAdmin } = require('../middleware/UserValidation');
 
-router.get('/', userCheck, checkAdmin, async (req, res) => {
-    const cvList = await cv.findAll();
-    if(cvList && (typeof(cvList.errno)!=='undefined')){
-        return res.sendStatus(500)
+const storageCv = multer.diskStorage(
+    {
+        destination: function (req, file, cb) {
+            cb(null, 'uploads/cvs')
+        },
+        filename: function (req, file, cb) {
+            const now = new Date(Date.now());
+            const jour = now.getDate()
+            const month = now.getMonth()
+            const year = now.getFullYear()
+            const filename = jour+"-"+month+"-"+year+"_"+file.originalname
+            cb(null, filename)
+        }
     }
-    if (cvList)
-    return res.status(200).json(cvList);
-    else
-    return res.sendstatus(500);
-});
+);
+
+const uploadCv = multer({ storage: storageCv });
+
+const addCvToDb = async (path) => {
+    //Envoi à la BDD
+    const result = await cv.create(path);
+
+    if (result && (typeof (result.errno) !== 'undefined')) {
+        return { error: 500 };
+    }
+    if (result) {
+        return { error: 0, newId: result };
+    }
+    else {
+        return { error: 500 };
+    }
+}
 
 router.get('/:id', userCheck, checkAdmin, (req, res) => {
     cv.findOne(req.params.id)
@@ -32,15 +55,52 @@ router.get('/:id', userCheck, checkAdmin, (req, res) => {
         })
 });
 
-router.post('/', (req, res) => {
-
+router.get('/', userCheck, checkAdmin, (req, res) => {
+    cv.findAll(req.params.id)
+        .then((cv) => {
+            if(cv && (typeof(cv.errno)!=='undefined')){
+                return res.sendStatus(500)
+            }
+            if (cv) {
+                res.status(200).json(cv)
+            }
+            else {
+                res.status(404).send('cvs not found')
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Internal Error');
+        })
 });
+
+router.post('/', userCheck, uploadCv.single('file'), async (req, res) => {
+    const path = req.file.path;
+    const addedDb = await addCvToDb(path);
+    const error = addedDb.error;
+    if (error === 0) {
+        const newCvId = addedDb.newId;
+        const is_owner = true
+        const id_users = req.userData.user_id
+        const addCv = await cv.add_CvToUser(id_users, newCvId, is_owner)
+        if(addCv && (typeof (addCv.errno) !== "undefined"))
+            return res.sendStatus(500);
+        else
+            return res.sendStatus(201);
+    }
+    else {
+        if (error === 422) {
+            return res.status(error).json(addedDb.errors)
+        }
+        return res.sendStatus(error);
+    }
+});
+
 router.put('/:id', (req, res) => {
 
 });
 router.delete('/:id', (req, res) => {
 
 });
-
 
 module.exports = router;
