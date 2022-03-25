cb753696e6f9557c5af71c7ff4f2f3519e632e6a
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-25 11:20:41 +0100

    adding get job type route

--- models/joboffers.model.js
+++ models/joboffers.model.js
@@ -3,6 +3,18 @@ const Joi = require('joi');
 
 const db = connection.promise();
 
+const getOfferTypeAll=()=>{
+    return db
+        .query("SELECT id_offer_type, name_offer FROM offer_type ORDER BY name_offer")
+        .then(([result])=>{
+            return result;
+        })
+        .catch((err)=>{
+            console.log(err);
+            return err;
+        })
+}
 
 module.exports = {
+    getOfferTypeAll,
 }
--- routes/joboffers.routes.js
+++ routes/joboffers.routes.js
@@ -2,20 +2,30 @@ const router = require("express").Router();
 const jobOffer = require('../models/joboffers.model');
 
 router.get('/', (req, res) => {
-
+    return res.sendStatus(402);
 });
-router.get('/:id', (req, res) => {
 
+router.get('/offertype/',async (req,res)=>{
+    const result=await jobOffer.getOfferTypeAll();
+    if (result && (typeof (result.errno) !== 'undefined')) {
+        return res.sendStatus(500);
+    }
+    return res.status(200).json(result);
+});
+
+router.get('/:id', (req, res) => {
+    return res.sendStatus(402);
 });
 router.post('/', (req, res) => {
-
+    return res.sendStatus(402);
 });
 router.put('/:id', (req, res) => {
-
+    return res.sendStatus(402);
 });
 router.delete('/:id', (req, res) => {
-
+    return res.sendStatus(402);
 });
+
 
 
 module.exports = router;
