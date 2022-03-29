557cefa6f28c4e48527f9da0493691ed04977e4a
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-29 11:08:31 +0200

    merging dev

--- db-config.js
+++ db-config.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  timezone:'Z',
+  timezone:'Z'
 });
 
 module.exports = connection;
